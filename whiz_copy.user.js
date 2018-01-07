452f44d3d240b077092d40c6947c430dc6ee5c4a
steppin <super.steppin@gmail.com>
2018-01-07 14:59:57 -0500

    Create whiz_copy.user.js

---
+++ whiz_copy.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         whiz_copy.user.js
+// @namespace    https://github.com/steppin/userscripts
+// @version      0.1
+// @description  Enable cut and copy operations on whizlabs.com exams
+// @author       Me
+// @match        https://learning.whizlabs.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    $("body").off("contextmenu");
+    $("body").unbind("cut copy");
+})();
