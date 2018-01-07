99076821cabd117a802bbbe0db6e351c962e68e9
steppin <super.steppin@gmail.com>
2018-01-07 15:20:40 -0500

    Update and rename whizCopy.user.js to whiz_copy.user.js

--- whiz_copy.user.js
+++ whiz_copy.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         whizCopy
+// @name         whiz_copy
 // @namespace    https://github.com/steppin/userscripts
 // @version      0.1
 // @description  Enable cut and copy operations on whizlabs.com exams
 // @author       Me
-// @license      GPL-3.0-or-later	
+// @license      // @license GPL-3.0+; http://www.gnu.org/licenses/gpl-3.0.txt
 // @match        https://learning.whizlabs.com/*
 // @grant        none
 // ==/UserScript==
